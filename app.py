f3342789cd1827d5d93077db58137d5bb75eafe3
Rishabh kumar <rishabhgdsc100@gmail.com>
2026-04-07 12:10:42 +0530

    Display predicted next word in UI
    Added result display for predicted next word.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 import os
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
